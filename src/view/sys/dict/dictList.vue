e19e466ae302ca324dfe77a257fe9bdaa9501800
githubyunhan <240791094@qq.com>
2022-01-15 21:16:34 +0800

    12.2 数据字典模块的布局：字典数据展示的设计

--- src/view/sys/dict/dictList.vue
+++ src/view/sys/dict/dictList.vue
@@ -12,7 +12,33 @@
       </div>
       <div style="clear: both;"></div>
       <div style="margin-top: 10px;">
-        这是我们的表格区域
+        <Table ref="dictTable" @on-sort-change="onSortChange"  :columns="columns" :data="dictData" :height="tableHeight">
+          <template slot-scope="{ row, index }" slot="dictCode">
+            <Input type="text" v-model="editDictCode" v-if="editIndex === index"/>
+            <span v-else>{{ row.dictCode }}</span>
+          </template>
+
+          <template slot-scope="{ row, index }" slot="dictText">
+            <Input type="text" v-model="editDictText" v-if="editIndex === index"/>
+            <span v-else>{{ row.dictText }}</span>
+          </template>
+
+          <template slot-scope="{ row, index }" slot="dictValue">
+            <Input type="text" v-model="editDictValue" v-if="editIndex === index"/>
+            <span v-else>{{ row.dictValue}}</span>
+          </template>
+
+          <template slot-scope="{ row, index }" slot="action">
+            <div v-if="editIndex === index">
+              <Button type="success" @click="handleUpdate(index)">确定</Button>
+              <Button type="error" @click="editIndex = -1">取消</Button>
+            </div>
+            <div v-else>
+              <Button type="primary" @click="handleEdit(row, index)" size="small">编辑</Button>
+              <Button type="error" @click="handleDelete(row, index)" size="small">删除</Button>
+            </div>
+          </template>
+        </Table>
       </div>
       <div style="margin-top: 10px;">
         这是我们的分页组件
@@ -26,7 +52,45 @@
     name: "dictList",
     data() {
       return {
-        search: ''
+        search: '',
+        dictData: [],
+        columns: [/*列*/
+          {
+            title: '字典类型',
+            key: 'dictType',
+            sortable: true
+          },
+          {
+            title: '字典编码',
+            slot: 'dictCode',
+            key: 'dictCode',
+            sortable: true
+          },
+          {
+            title: '字典文本',
+            slot: 'dictText',
+            key: 'dictText',
+            sortable: true
+          },
+          {
+            title: '字典数值',
+            slot: 'dictValue',
+            key: 'dictValue',
+            sortable: true
+          },
+          {
+            title: '操作',
+            slot: 'action'
+          }
+        ],
+        key:'dictType',
+        order:'desc',
+        editIndex: -1,  // 当前聚焦的输入框的行数
+        editId: '',
+        editDictCode: '',
+        editDictText: '',
+        editDictValue: '',
+        tableHeight:200
       }
     },
     methods: {
@@ -35,7 +99,31 @@
       },
       handleSearch(){
         console.log('这是一个查询')
+      },
+      handleUpdate(index){
+        console.log('点击了确定')
+      },
+      handleEdit(row, index){
+        console.log('点击了编辑')
+      },
+      handleDelete(row, index){
+        console.log('点击了删除')
+      },
+      onSortChange(sort){
+        console.log(sort.key+'-'+sort.order)
+        if(sort.order=='normal'){
+          this.order = '';
+        }else{
+          this.key = sort.key;
+          this.order = sort.order;
+        }
+        this.handleSearch();
       }
+    },
+    mounted() {
+      // 初始化完成组件的时候执行以下的逻辑
+      this.handleSearch();
+      this.tableHeight = window.innerHeight - this.$refs.dictTable.$el.offsetTop - 270
     }
   }
 </script>
