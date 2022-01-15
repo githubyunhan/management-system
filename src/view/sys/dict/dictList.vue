782685fe2a06c858b0c2b68f057de211a1e9dcac
githubyunhan <240791094@qq.com>
2022-01-15 20:03:57 +0800

    11.卡片布局的引入（Card标签）

--- src/view/sys/dict/dictList.vue
+++ src/view/sys/dict/dictList.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-    这是数据字典的列表
+    <Card title="字典管理">
+      <div>
+        <div style="display: inline-block;float: left">
+          这是一个按钮
+        </div>
+        <div style="display: inline-block;float: right">
+          这是一个搜索框
+        </div>
+      </div>
+      <div style="clear: both;"></div>
+      <div style="margin-top: 10px;">
+        这是我们的表格区域
+      </div>
+      <div style="margin-top: 10px;">
+        这是我们的分页组件
+      </div>
+    </Card>
   </div>
 </template>
 
