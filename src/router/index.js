65ee0e20c9f265aecd4fcd7651f65ce81bb08062
githubyunhan <240791094@qq.com>
2022-01-15 19:36:24 +0800

    10.前端路由的简单实现：数据字典页面的展示

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+//import main from '../view/main/main.vue';
+/*懒加载*/
+const main=()=>import('../view/main/main.vue')
 /*1.导入路由对象*/
 Vue.use(Router);
 
@@ -26,9 +28,38 @@ const routes = [
     component: resolve => {
       require(['../view/main/main.vue'], resolve);
     },
-    meta:{
-      title:'主页'
+    meta: {
+      icon: 'ios-settings',
+      title: '系统首页',
+      hideInMenu: true,
+      requireAuth: false //表示当前响应的请求是否需要进行登录拦截验证【true：需要；false：不需要】
     }
+  },
+  {
+    path: '/sys',
+    name: 'sys',
+    component: main,
+    meta: {
+      icon: 'ios-construct',
+      title: '系统管理',
+      code:'system-manage',
+      requireAuth: true //表示当前响应的请求是否需要进行登录拦截验证【true：需要；false：不需要】
+    },
+    children:[
+      {
+        path: 'dictList',
+        name: 'dictList',
+        component: resolve => {
+          require(['../view/sys/dict/dictList.vue'], resolve);
+        },
+        meta: {
+          icon: 'ios-paper',
+          title: '字典维护',
+          code:'system-manage-dict',
+          requireAuth: true //表示当前响应的请求是否需要进行登录拦截验证【true：需要；false：不需要】
+        }
+      }
+    ]
   }
 ];
 
---
+++ src/view/sys/dict/dictList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    这是数据字典的列表
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "dictList"
+  }
+</script>
+
+<style scoped>
+
+</style>
