113a592019be4907b687d020a74e6bd06976ce65
githubyunhan <240791094@qq.com>
2022-01-15 20:53:52 +0800

    12.1 数据字典模块的布局：添加新增字典按钮与搜索框

--- src/view/sys/dict/dictList.vue
+++ src/view/sys/dict/dictList.vue
@@ -2,11 +2,12 @@
   <div>
     <Card title="字典管理">
       <div>
-        <div style="display: inline-block;float: left">
-          这是一个按钮
+        <div style="display:inline-block;float:left;">
+          <Button type="success" @click="addDict">+新增字典</Button>
         </div>
-        <div style="display: inline-block;float: right">
-          这是一个搜索框
+        <div style="display:inline-block;float:right;">
+          <Input v-model="search" suffix="ios-search" placeholder="请输入查询信息" style="width: auto"
+                 :search=true @on-search="handleSearch"/>
         </div>
       </div>
       <div style="clear: both;"></div>
@@ -22,7 +23,20 @@
 
 <script>
   export default {
-    name: "dictList"
+    name: "dictList",
+    data() {
+      return {
+        search: ''
+      }
+    },
+    methods: {
+      addDict() {
+        console.log('增加字典')
+      },
+      handleSearch(){
+        console.log('这是一个查询')
+      }
+    }
   }
 </script>
 
